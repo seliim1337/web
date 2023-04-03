0dca5e4cf371fd63da769443c6d8d2f20676b227
sel <124797777+seliim1337@users.noreply.github.com>
2023-04-03 04:28:27 +0300

    Update AboutCard.tsx

--- src/components/card/AboutCard.tsx
+++ src/components/card/AboutCard.tsx
@@ -2,7 +2,7 @@ export function AboutCard() {
   return (
     <div className="mb-3">
       <h2 className="font-bold text-xs text-slate-300 leading-4 mb-2 uppercase select-none">About Me</h2>
-      <div className="whitespace-pre-line text-normal">{' kanımız biz gibi '}</div>
+      <div className="whitespace-pre-line text-normal">{'  bav çika bav bav \naşkım böyle söylerrr (düzenlendi) \n mav mav mavv  '}</div>
     </div>
   );
 }
