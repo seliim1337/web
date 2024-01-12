c47b26ecbf8aa5aabe83e2492ac461d3c613b9fe
sel <124797777+seliim1337@users.noreply.github.com>
2024-01-12 11:50:36 +0300

    Add files via upload

--- src/components/card/BadgesCard.tsx
+++ src/components/card/BadgesCard.tsx
@@ -1,11 +1,18 @@
 import { Tooltip } from 'react-tooltip';
+import '../activity/tooltip.css';
 export function BadgesCard() {
 
   return (
-    <div className="absolute top-[130px] right-[20px] justify-end select-none">
-          <Tooltip id="tooltip" style={{ backgroundColor: "rgb(0, 0, 0)", color: "#fff" }} place="left"/>
+    <div className="absolute top-[130px] right-[10px] justify-end select-none" id="badges-background">
+          <Tooltip 
+           id="badges-tooltip" 
+           style={{ 
+           backgroundColor: "rgb(0, 0, 0)", 
+           color: "#fff" }} 
+           place="left"
+           />
           <img
-            data-tooltip-id="tooltip" 
+            data-tooltip-id="badges-tooltip" 
             data-tooltip-content="Hypesquad Balance"
             src="/assets/discord-hypesquad-balance.svg"
             alt="discord-hypesquad-balance"
@@ -15,7 +22,7 @@ export function BadgesCard() {
             draggable="false"
           />
           <img
-            data-tooltip-id="tooltip" 
+            data-tooltip-id="badges-tooltip" 
             data-tooltip-content="Active Developer"
             src="/assets/discord-active-developer.svg"
             alt="discord-active-developer"
@@ -23,8 +30,10 @@ export function BadgesCard() {
             height="22"
             className="inline-block"
             draggable="false"
-          />          
-          {/*<img
+          />        
+          {/*<img          
+            data-tooltip-id="badges-tooltip" 
+            data-tooltip-content="Subscriber since 19 Oct 2019"
             src="/assets/discord-nitro.svg"
             alt="discord-nitro"
             width="22"
@@ -33,6 +42,8 @@ export function BadgesCard() {
             draggable="false"
           />
           <img
+            data-tooltip-id="badges-tooltip" 
+            data-tooltip-content="Server Boosting since 19 Oct 2019"
             src="/assets/discord-booster.svg"
             alt="discord-booster"
             width="22"
@@ -41,7 +52,7 @@ export function BadgesCard() {
             draggable="false"
           />
           <img
-            data-tooltip-id="tooltip" 
+            data-tooltip-id="badges-tooltip" 
             data-tooltip-content="Originally known as selim#0697"
             src="/assets/originally-name.png"
             alt="discord-booster"
@@ -49,8 +60,8 @@ export function BadgesCard() {
             height="22"
             className="inline-block"
             draggable="false"
-          />
-          */}
+          />*/}
+         
           
         </div>
   );
