a2a91d5cede1bff5fdcfae5748cf3ccbd46162b9
sel <124797777+seliim1337@users.noreply.github.com>
2024-01-27 17:32:38 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ function App() {
       {status ? (
         <ProfileCard data={status} />
       ) : (
+        <span className="loader"></span>
+        /*
         <svg className="animate-spin" width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
           <g fill="none" fillRule="evenodd">
             <g transform="translate(1 1)" strokeWidth="2">
@@ -22,6 +24,7 @@ function App() {
             </g>
           </g>
         </svg>
+        */
       )}
     </div>
   );
